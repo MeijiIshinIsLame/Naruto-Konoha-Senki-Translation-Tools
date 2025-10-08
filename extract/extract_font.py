e0262c0fd445a880d3c0918b2ad93dfd656b08ec
meijiishinislame <meijiishinislame@gmail.com>
2025-10-08 13:27:38 -1000

    added font insertion

--- extract/extract_font.py
+++ extract/extract_font.py
@@ -1,17 +1,26 @@
 import io
+import shutil
 from pathlib import Path
 from PIL import Image, ImageDraw
 
-def load_sjis_table(filename=Path("../config/sjis-utf8-1byte.tbl")):
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
+WHITE = (255, 255, 255)
+
+def load_sjis_table(path=Path("config/sjis-utf8-1byte.tbl")):
     sjis = {}
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         for line in f:
-            hv, char = line.split("=")
-            sjis[hv] = char
+            try:
+                hv, char = line.split("=", 1)
+                sjis[hv] = char
+            except ValueError:
+                print(f"ValueError: skipping {line}")
+    print(sjis)
     return sjis
 
 #extract the character bytes from ROM
- def extract_char_bytes(f, hv):
+def extract_char_bytes(f, hv):
     start_address = 0x9CD54
     initial_offset = 32
     hv = int(hv, 16)
@@ -27,36 +36,41 @@ def draw_image(path, b):
     height = 8
     img = Image.new("RGB", (width, height), "white")
     pixels = img.load()
-    byte_to_color = {"11": (0, 0, 0),
-                     "01": (128, 128, 128),
-                     "00": (255, 255, 255),
-                     "10": (0, 0, 0)}
+    byte_to_color = {
+        "11": BLACK,
+        "01": GRAY,
+        "00": WHITE,
+        "10": BLACK
+    }
+    j = 0
     for i, one_byte in enumerate(b):
         eight_bits = one_byte
         bits = f"{one_byte:08b}"
-        rev_bits = bits[::-1] #reversing because its loaded backwards
-        chunks = [rev_bits[:2],
-                  rev_bits[2:4],
-                  rev_bits[4:6],
-                  rev_bits[6:8]]
+        chunks = [
+            bits[:2],
+            bits[2:4],
+            bits[4:6],
+            bits[6:8]
+        ]
+        chunks = list(reversed(chunks))
         for bpair in chunks:
-            x = i % width
-            y = i // width
+            x = j % width
+            y = j // width
             pixels[x, y] = byte_to_color[bpair]
+            j+=1
     img.save(path)   
         
 '''
 Extract all chars and save to folder as separate images
 '''
-def extract_chars_and_draw(game_path,
-                           out_path=Path("font"),
-                           sjis_tbl_path=Path("../config/sjis-utf8-1byte.tbl")):
+def extract_chars_and_draw(game_path, out_path=Path("extract/font"), sjis_tbl_path=Path("config/sjis-utf8-1byte.tbl")):
     out_path.mkdir(parents=True, exist_ok=True)
     sjis = load_sjis_table(sjis_tbl_path)
-    with open(game_path, rb) as f:
-        for k, v in enumerate(sjis):
+    with open(game_path, "rb") as f:
+        for k, v in sjis.items():
             print(f"Extracting sjis {k} as {v}...")
             b = extract_char_bytes(f, k)
+            #with open(Path(out_path) / f"{k}.bin", "wb") as f2: f2.write(b)
             draw_image(Path(out_path) / f"{k}.bmp", b)
             print("Extraction Complete!")
     print("Finished!")
---
+++ inject/inject_font.py
@@ -0,0 +1,90 @@
+import io
+import shutil
+from pathlib import Path
+
+BLACK = "11"
+GRAY = "01"
+WHITE = "00"
+
+'''
+Swap each pair in the list because BMP is left to right starting at the bottom,
+but the game renders right to left from bottom.
+'''
+def swap_pairs(lst):
+    swapped = lst[:]
+    for i in range(0, len(swapped) - 1, 2):
+        swapped[i], swapped[i + 1] = swapped[i + 1], swapped[i]
+    return swapped
+
+def convert_bmp_bytes(path):
+    #file is always 246 bytes no matter what
+    start = 0x36
+    end = 0xF5
+    bitpairs = []
+    
+    colorconverter = {
+        bytes.fromhex("000000"): BLACK,
+        bytes.fromhex("808080"): GRAY,
+        bytes.fromhex("FFFFFF"): WHITE
+    }
+    with open(path, "rb") as f:
+        f.seek(start)
+        while f.tell() <= end:
+            color = f.read(3)
+            bitpair = colorconverter[color]
+            bitpairs.append(bitpair)
+    # reverse the 4 bitpairs before joining. The binary is read kinda like little endian.
+    binary = [
+        "".join(bitpairs[i:i+4][::-1])
+        for i in range(0, len(bitpairs), 4)
+    ]
+    # now that the bytes are created, it must be reversed and pair swapped to render right to left
+    flipped = swap_pairs(binary[::-1])
+    byte_data = bytes(int(b, 2) for b in flipped) #make it a single bytes object so we can shove it in the ROM.
+    return byte_data
+    
+def calculate_insert_position(sjis):
+    start_address = 0x9CD54
+    initial_offset = 32
+    final_offset = (sjis - initial_offset) << 4
+    start = start_address + final_offset
+    return start
+    
+def inject_font(
+    input_rom_path=None,
+    font_path=Path("inject/font"),
+    output_rom_path=Path("game/Naruto - Konoha Senki English translation.gba")
+):
+    try:
+        fontfiles = [
+            f for f in font_path.iterdir()
+            if f.is_file() and f.suffix.lower() == ".bmp"
+        ]
+        if not fontfiles:
+            raise ValueError(f"No BMP files in font path {font_path}. Each character needs to be a 8x8 BMP image.")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Font folder {font_path} not found.")
+    except NotADirectoryError:
+        raise NotADirectoryError(f"{font_path} is not a directory.")
+        
+    rom = Path()
+    if output_rom_path.exists():
+        rom = output_rom_path
+    else:
+        if input_rom_path is None:
+            raise ValueError("No input ROM path provided and output ROM doesn't exist. Put a ROM in ./game or use argument --input_rom_path")
+        output_rom_path.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(input_rom_path, output_rom_path)
+        rom = output_rom_path
+        
+    for fontfile in fontfiles:
+        b = convert_bmp_bytes(fontfile)
+        sjis = int(fontfile.stem, 16)
+        start = calculate_insert_position(sjis)
+        print(f"inserting {hex(sjis)} at 0x{hex(start)}")
+        with open(rom, "r+b") as f:
+            f.seek(start)
+            f.write(b)
+    print("Finished")
+    
+        
