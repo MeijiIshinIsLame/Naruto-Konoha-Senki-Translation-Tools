0ef91c979115fa39f890a173142a704afa9576e7
meijiishinislame <meijiishinislame@gmail.com>
2025-10-22 15:53:27 -1000

    starting to inject name labels

--- cli.py
+++ cli.py
@@ -8,6 +8,7 @@ from extract.extract_name_labels import extract_name_labels
 from inject.inject_font import inject_font
 from inject.inject_dialog_scripts import inject_dialog_scripts
 from inject.inject_entities import inject_entities
+from inject.inject_name_labels import inject_name_labels
 from inject.inject_asm import prepare_and_inject_asm
 
 def parse_args():
@@ -21,14 +22,14 @@ def parse_args():
     extract_font_parser.add_argument("--sjis_table", "-s", help="Optional. Path to the SJIS table file. Can be an exact or relative path.")
     
     #---------------------------- EXTRACT NAMES AND DESCRIPTIONS (items/moves) --------------
-    extract_font_parser = subparsers.add_parser("extract_entities", help="Extract names and descriptions of characters and items to ./extract/entities")
-    extract_font_parser.add_argument("--rompath", "-i", help="Path of the ROM to extract from. Can be an exact or relative path.")
-    extract_font_parser.add_argument("--output_folder", "-o", help="Optional. Path to save font text files to. Can be an exact or relative path.")
+    extract_entities_parser = subparsers.add_parser("extract_entities", help="Extract names and descriptions of characters and items to ./extract/entities")
+    extract_entities_parser.add_argument("--rompath", "-i", help="Path of the ROM to extract from. Can be an exact or relative path.")
+    extract_entities_parser.add_argument("--output_folder", "-o", help="Optional. Path to save font text files to. Can be an exact or relative path.")
     
     #---------------------------- EXTRACT NAME LABELS (character dialog names and places) --------------
-    extract_font_parser = subparsers.add_parser("extract_name_labels", help="Extract names of characters and places in dialog scripts to ./extract/name_labels")
-    extract_font_parser.add_argument("--rompath", "-i", help="Path of the ROM to extract from. Can be an exact or relative path.")
-    extract_font_parser.add_argument("--output_folder", "-o", help="Optional. Path to save font text files to. Can be an exact or relative path.")
+    extract_namelabels_parser = subparsers.add_parser("extract_name_labels", help="Extract names of characters and places in dialog scripts to ./extract/name_labels")
+    extract_namelabels_parser.add_argument("--rompath", "-i", help="Path of the ROM to extract from. Can be an exact or relative path.")
+    extract_namelabels_parser.add_argument("--output_folder", "-o", help="Optional. Path to save font text files to. Can be an exact or relative path.")
     
     #---------------------------- INJECT FONT ---------------------------------
     inject_font_parser = subparsers.add_parser("inject_font", help="Takes a folder of 8x8 BMP images with their names as SJIS hex, and overwrites the character graphics in the ROM.")
@@ -44,12 +45,19 @@ def parse_args():
     inject_dialog_parser.add_argument("--output_rompath", "-o", help="Optional unless there is no output ROM yet and no input rompath specified. Path of the output rom file. Can be an exact or relative path.")
     inject_dialog_parser.add_argument("--overwrite_output_rom", "-w",  action='store_true', help="Optional. Overwrite output ROM with input ROM.")
     
-    #---------------------------- INJECT ENTITIES ---------------------------------
+    #---------------------------- INJECT ENTITIES NAMES AND DESCRIPTIONS (items/moves) ---------------------------------
     inject_entities_parser = subparsers.add_parser("inject_entities", help="Takes a folder of entities, convert them to binary, replace their pointers, and insert the pointers + dialogs into ROM.")
     inject_entities_parser.add_argument("--input_rompath", "-i", help="Optional unless there is no output ROM yet. Path of the ROM to copy into the new ROM. Can be an exact or relative path.")
     inject_entities_parser.add_argument("--entities_path", "-f", help="Optional. Path of folder with dialogs to inject. Can be an exact or relative path.")
     inject_entities_parser.add_argument("--output_rompath", "-o", help="Optional unless there is no output ROM yet and no input rompath specified. Path of the output rom file. Can be an exact or relative path.")
     inject_entities_parser.add_argument("--overwrite_output_rom", "-w",  action='store_true', help="Optional. Overwrite output ROM with input ROM.")
+    
+    #---------------------------- INJECT NAME LABELS (character dialog names and places) ---------------------------------
+    inject_namelabels_parser = subparsers.add_parser("inject_name_labels", help="Takes a folder of name labels, and inserts them into ROM")
+    inject_namelabels_parser.add_argument("--input_rompath", "-i", help="Optional unless there is no output ROM yet. Path of the ROM to copy into the new ROM. Can be an exact or relative path.")
+    inject_namelabels_parser.add_argument("--labels_path", "-f", help="Optional. Path of folder with dialogs to inject. Can be an exact or relative path.")
+    inject_namelabels_parser.add_argument("--output_rompath", "-o", help="Optional unless there is no output ROM yet and no input rompath specified. Path of the output rom file. Can be an exact or relative path.")
+    inject_namelabels_parser.add_argument("--overwrite_output_rom", "-w",  action='store_true', help="Optional. Overwrite output ROM with input ROM.")
     
     #---------------------------- INJECT ASM ---------------------------------    
     inject_asm_parser = subparsers.add_parser("inject_asm", help="Takes a ROM and patches is with the files located in the 'inject' folder.")
@@ -119,7 +127,7 @@ def parse_args():
         kwargs = {}
         if args.input_rompath:
             kwargs["input_rompath"] = Path(args.input_rompath)
-        if args.scripts_path:
+        if args.entities_path:
             kwargs["entities_path"] = Path(args.entities_path)
         if args.output_rompath:
             kwargs["output_rompath"] = Path(args.output_rompath)
@@ -127,7 +135,22 @@ def parse_args():
             #try to get output path from kwargs, otherwise use default
             dest = kwargs.get("output_rompath", defaults.OUTPUT_ROM)
             helpers.overwrite_output_rom(source=kwargs["input_rompath"], dest=dest)
-        inject_dialog_scripts(**kwargs)
+        inject_entities(**kwargs)
+        
+    if args.command == "inject_name_labels":
+        kwargs = {}
+        if args.input_rompath:
+            kwargs["input_rompath"] = Path(args.input_rompath)
+        if args.labels_path:
+            kwargs["labels_path"] = Path(args.labels_path)
+        if args.output_rompath:
+            kwargs["output_rompath"] = Path(args.output_rompath)
+        if args.overwrite_output_rom:
+            #try to get output path from kwargs, otherwise use default
+            dest = kwargs.get("output_rompath", defaults.OUTPUT_ROM)
+            helpers.overwrite_output_rom(source=kwargs["input_rompath"], dest=dest)
+        inject_name_labels(**kwargs)
+        
         
     if args.command == "inject_asm":
         kwargs = {}
@@ -155,4 +178,5 @@ def parse_args():
         inject_font(**kwargs)
         inject_dialog_scripts(**kwargs)
         inject_entities(**kwargs)
+        inject_name_labels(**kwargs)
         prepare_and_inject_asm(**kwargs)
---
+++ inject/inject_name_labels.py
@@ -0,0 +1,51 @@
+import io
+from pathlib import Path
+from tqdm import tqdm
+from classes.pointers import Pointer, PointerList
+from config import defaults
+from utils import helpers
+
+START = 0x599280
+END = 0x599924 #possibly 24 check after
+EXIT_BYTE = b'\x00'
+insert_path = Path("inject/name_labels")
+
+def get_sjis_and_zeros(f):
+    """get a bytes object of SJIS and zeros per name"""
+    b = f.read(1)
+    data = b
+    while b != EXIT_BYTE:
+        data += b
+        b = f.read(1) 
+    while b == EXIT_BYTE:
+        data += b 
+        b = f.read(1)
+    return data
+    
+
+def inject_name_labels(input_rompath=None, output_rompath=defaults.OUTPUT_ROM, files=Path("inject/name_labels")):
+    """check if name bytes can be injected without disruptig offset,
+       then inject."""
+    print("Inserting name labels")
+    rom = helpers.ensure_output_rompath(input_rompath, output_rompath)
+    files = helpers.get_files(insert_path, ".txt")
+    with open(rom, "r+b") as f:
+        for file in tqdm(files):
+            start = int(file.stem, 16)
+            f.seek(start)
+            test = get_sjis_and_zeros(f)
+            maxlen = len(get_sjis_and_zeros(f))
+            filesize = file.stat().st_size
+            if filesize <= maxlen:
+                f.seek(start)
+                with open(file, "rb") as f2:
+                    data = f2.read()
+                    zeros = EXIT_BYTE * (maxlen - len(data))
+                    data += zeros
+                    f.write(data)
+            else:
+                print(f"File {file.stem} cannot be written because it is too long\n filesize: {filesize}, maxlen: {maxlen}")
+                print(test)
+            
+    
+    
