1c9b7fd6733d172e8c0bfab421ceda2a1583726c
Zachary Silva <MeijiIshinislame@gmail.com>
2025-10-25 00:30:05 -1000

    ASM Hacking not working yet

--- inject/asm/testhack.asm
+++ inject/asm/testhack.asm
@@ -3,33 +3,59 @@
 
 //============== HOOK ======================
 	
-.org 0x080662fa
+.org 0x0806627e
 	bl namelabel_check
 	pop {r6, r7}
 
-.org 0x0809d13a
+.org 0x0809e700
 namelabel_check:
 	push {r6, r7}
 	ldr r6, =lower_addr
 	ldr r6, [r6]
 	ldr r7, =higher_addr
 	ldr r7, [r7]
-	cmp r0, r7
-	bgt jump_to_lr
-	cmp r0, r6
-	bgt big_text
+	cmp r2, r7
+	bge jump_to_lr
+	cmp r2, r6
+	bge big_text
 	bx lr
 
 big_text:
+	push {r3}
 	mov r1, 82h
-	ldr r0,[r0]
-	mov r3, 20h
-	add r0, r0, r3
+	ldrb r0,[r2]
+	cmp r0, 40h
+	blt jump_to_lr
+	cmp r0, 5Ah
+	blt make_uppercase_zenkaku_sjis
+	cmp r0, 60h
+	blt jump_to_lr
+	cmp r0, 7Ah
+	blt make_lowercase_zenkaku_sjis
 	bx lr
 
 jump_to_lr:
 	bx lr
+
+make_lowercase_zenkaku_sjis:
+	mov r3, 20h
+	add r0, r0, r3
+	lsl r1, r1, 8h
+	pop {r3}
+	push {r9}
+	pop {r7}
+	pop {r6}
+	bl 0x08066300
 	
+make_uppercase_zenkaku_sjis:
+	mov r3, 1Fh
+	add r0, r0, r3
+	lsl r1, r1, 8h
+	pop {r3}
+	push {r9}
+	pop {r7}
+	pop {r6}
+	bl 0x08066300
 
 .pool
 lower_addr:
--- inject/testhack.asm
+++ inject/testhack.asm
@@ -2,14 +2,58 @@
 .open "{source}","{dest}",0x08000000 
 
 //============== HOOK ======================
-.org 0x0806611e       
-    add r5, r5, 5h
-	add r5, r5, 5h
-	add r5, r5, 5h
-	add r5, r5, 5h
-	add r5, r5, 5h
-	add r5, r5, 5h
-	add r5, r5, 5h
-	add r5, r5, 5h
+	
+.org 0x080662fa
+	bl namelabel_check
+	pop {r6, r7}
+
+.org 0x0809e700
+namelabel_check:
+	push {r6, r7}
+	ldr r6, =lower_addr
+	ldr r6, [r6]
+	ldr r7, =higher_addr
+	ldr r7, [r7]
+	cmp r0, r7
+	bge jump_to_lr
+	cmp r0, r6
+	bge big_text
+	bx lr
+
+big_text:
+	push {r3}
+	mov r1, 82h
+	ldrb r0,[r0]
+	cmp r0, 40h
+	blt jump_to_lr
+	cmp r0, 5Ah
+	blt make_uppercase_zenkaku_sjis
+	cmp r0, 60h
+	blt jump_to_lr
+	cmp r0, 7Ah
+	blt make_lowercase_zenkaku_sjis
+	bx lr
+
+jump_to_lr:
+	bx lr
+
+make_lowercase_zenkaku_sjis:
+	mov r3, 20h
+	add r0, r0, r3
+	pop {r3}
+	bx lr
+	
+make_uppercase_zenkaku_sjis:
+	mov r3, 1Fh
+	add r0, r0, r3
+	pop {r3}
+	bx lr
+
+.pool
+lower_addr:
+	.word 0x085a57d0
+
+higher_addr:
+	.word 0x085a632f
 
 .close
