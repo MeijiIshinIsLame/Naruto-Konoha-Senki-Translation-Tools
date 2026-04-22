25c9769fa35c20e77702c2f38f4ccb523d519352
Zachary Silva <MeijiIshinislame@gmail.com>
2026-04-22 00:50:31 -1000

    got control of location select menu width

--- inject/asm/bigger_menu.asm
+++ inject/asm/bigger_menu.asm
@@ -1,9 +1,14 @@
 .gba
 .open "{source}","{dest}",0x08000000 
 
-//============== Change Menu Width ======================
+//============== Change Main Menu Width ======================
 .org 0x0808b9d6
 	mov r1, 0xa ;x position to start drawing
 	mov r3, 0xc ;x width
 
+//============== Change Location Select Menu Width ======================
+.org 0x08009a9b
+	.byte 0x10 ;x position to start drawing
+	.byte 0x00 ;padding
+	.byte 0xd ;x width	
 .close
